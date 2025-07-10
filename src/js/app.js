739e7c1a88d10fab2f0d4e9d5be94801e17588c5
Sorokovsky <Sorokovskys@ukr.net>
2025-07-10 19:25:08 +0300

    hero block

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 import { isWebp, enableBurgerMenu } from "./modules/functions.js";
 isWebp();
 enableBurgerMenu();
+import Swiper from "swiper";
+import { Autoplay } from 'swiper/modules';
+new Swiper('.hero__slider', {
+    modules: [Autoplay],
+    direction: 'horizontal',
+    loop: true,
+    allowTouchMove: false,
+
+    autoplay: {
+        delay: 5000,
+    },
+});
